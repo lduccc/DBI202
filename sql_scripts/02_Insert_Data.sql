34a8ef31a476c6c7c1cc22c306d3b99b366dd1a5
lduccc <lducc.work@gmail.com>
2025-07-22 17:47:54 +0700

    Update 02_Insert_Data.sql

--- sql_scripts/02_Insert_Data.sql
+++ sql_scripts/02_Insert_Data.sql
@@ -1,3 +1,15 @@
+/*For testing
+SELECT * FROM Teacher ORDER BY id;
+SELECT * FROM Student ORDER BY id;
+SELECT * FROM Course ORDER BY id;
+SELECT * FROM Course_Material ORDER BY id;
+SELECT * FROM Class ORDER BY course_id, id;
+SELECT * FROM Class_Student ORDER BY class_id, student_id;
+SELECT * FROM Exam ORDER BY class_id, date;s
+SELECT * FROM Exam_Result ORDER BY student_id, exam_id;
+SELECT * FROM Payment ORDER BY id;
+*/
+
 USE EducateDB
 GO
 
